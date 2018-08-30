964d5edeb2568f3ce3d6ffac1eb48d3ee3cdfec1
Sara Harvey <saraharveynyc@gmail.com>
2018-08-30 15:46:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,27 +11,7 @@ end
 fido = Dog.new("Fido")
 fido.name
 
-class Dog
- 
-  def initialize(name)
-    @name = name
-  end
- 
-  def name
-    @name
-  end
- 
-  def name=(new_name)
-    @name = new_name
-  end
- 
-end
 
-fido = Dog.new("Fido")
-fido.name
-
-fido.name = "Fido"
-fido.name
 
 
 
